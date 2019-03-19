a4322a81237d87a326cc6915c1c86da6b60695a6
Yesid Perea <yesid.perea@outlook.com>
2019-03-18 21:56:36 -0500

    Adding OrgChartEdit.php and OrgChartView.php view files and implement their calling in onViewOutput and onEditOutput methods in Orgchart.php class, to separate presentation logic from pluslet object.

---
+++ lib/SubjectsPlus/Control/Pluslet/views/OrgChartEdit.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Presentation Logic/HTML for the edit version of OrgChart Pluslet
+ * User: yperea
+ * Date: 3/18/19
+ */
+?>
+
+<h1>This is the Edit</h1>
---
+++ lib/SubjectsPlus/Control/Pluslet/views/OrgChartView.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Presentation Logic/HTML for the view version of OrgChart Pluslet
+ * User: yperea
+ * Date: 3/18/19
+ */
+?>
+
+<h1>This is the View</h1>
