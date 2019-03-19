9645a1692d7d176712d51ceadaeb33b6618f2121
Yesid Perea <yesid.perea@outlook.com>
2019-03-18 22:54:59 -0500

    Implementing function getStaff() in OrgChart.php class and implement it on both views for testing.

--- lib/SubjectsPlus/Control/Pluslet/OrgChart.php
+++ lib/SubjectsPlus/Control/Pluslet/OrgChart.php
@@ -8,12 +8,17 @@ require_once ("Pluslet.php");
  */
 class Pluslet_OrgChart extends Pluslet {
 
+    protected $_staff;
+
+
     public function __construct($pluslet_id = "", $flag = "", $subject_id = "", $isclone = 0) {
 
         parent::__construct($pluslet_id, $flag, $subject_id, $isclone);
 
         $this->_title = "Organizational Chart";
         $this->_type = "OrgChart";
+        $this->_staff = $this->getStaff();
+
 
     }
 
@@ -49,5 +54,31 @@ class Pluslet_OrgChart extends Pluslet {
 
         $this->_body = $this->loadHtml(__DIR__ . '/views/OrgChartEdit.php');
 
+    }
+
+
+    protected function getStaff() {
+
+        $querier = new Querier();
+        $staff=null;
+
+        $sql = "SELECT DISTINCT s.supervisor_id,
+                s.staff_id,
+                s.lname,
+                s.fname,
+                s.title,
+                s.department_id,
+                d.name as department,
+                s.staff_sort,
+                s.active
+                FROM staff s
+                INNER JOIN staff_department sd ON s.department_id = sd.department_id
+                INNER JOIN department d ON sd.department_id = d.department_id
+                ORDER BY s.supervisor_id, s.staff_id
+                LIMIT 5";
+
+        $staff = $querier->query($sql);
+
+        return $staff;
     }
 }
--- lib/SubjectsPlus/Control/Pluslet/views/OrgChartView.php
+++ lib/SubjectsPlus/Control/Pluslet/views/OrgChartView.php
@@ -7,3 +7,9 @@
 ?>
 
 <h1>This is the View</h1>
+
+<?php foreach($this->_staff as $staff): ?>
+    <div class="subject-specialist-form">
+        <h4><?= $staff["lname"]; ?></h4>
+    </div>
+<?php endforeach; ?>
